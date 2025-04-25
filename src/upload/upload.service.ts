dfe2269812d5d0fef074f514f2cfe19bdc21aa15
Bakary <var.bsan@gmail.com>
2025-04-25 14:17:34 +0200

    chore

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -5,7 +5,6 @@ import {
 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { Injectable } from '@nestjs/common'
-import 'dotenv/config'
 
 @Injectable()
 export class UploadService {
